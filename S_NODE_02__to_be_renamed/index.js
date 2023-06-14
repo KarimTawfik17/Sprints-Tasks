098525e1f903a60b0fad8e3d13db2fa23750fe46
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-14 20:30:51 +0300

    add categorize functionality

--- S_NODE_02__to_be_renamed/index.js
+++ S_NODE_02__to_be_renamed/index.js
@@ -3,3 +3,19 @@ const STORE_API = "https://api.escuelajs.co/api/v1/products";
 async function getProducts() {
   return fetch(STORE_API).then((res) => res.json());
 }
+
+function categorize(products) {
+  return Object.values(
+    products.reduce((categories, product) => {
+      if (!categories[product.category.id]) {
+        categories[product.category.id] = {
+          category: { id: product.category.id, name: product.category.name },
+          products: [],
+        };
+      }
+      categories[product.category.id].products.push(product);
+      return categories;
+    }, {})
+  );
+}
+getProducts().then(categorize).then(console.log);
