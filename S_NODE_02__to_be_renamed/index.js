3c1c4c293dd2c22f8cbce04b92fed093ae56590e
Karim Tawfik <karim.tawfik17@gmail.com>
2023-06-14 21:07:35 +0300

    add converting price for all functionality

--- S_NODE_02__to_be_renamed/index.js
+++ S_NODE_02__to_be_renamed/index.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const STORE_API = "https://api.escuelajs.co/api/v1/products";
-
+const API_KEY = process.env.API_KEY;
 async function getProducts() {
   return fetch(STORE_API).then((res) => res.json());
 }
@@ -18,4 +19,27 @@ function categorize(products) {
     }, {})
   );
 }
-getProducts().then(categorize).then(console.log);
+
+async function getRate() {
+  const api = `https://api.currencyapi.com/v3/latest?apikey=${API_KEY}&currencies=EGP`;
+  const rate = await fetch(api)
+    .then((Response) => Response.json())
+    .then((json) => +json.data.EGP.value);
+  return rate;
+}
+
+async function transformProductsPrice(categories) {
+  const rate = await getRate();
+  return categories.map((category) => ({
+    ...category,
+    products: category.products.map((product) => ({
+      ...product,
+      price: (product.price * rate).toFixed(2),
+    })),
+  }));
+}
+
+getProducts()
+  .then(categorize)
+  .then(transformProductsPrice)
+  .then((data) => console.log(JSON.stringify(data, null, 2)));
