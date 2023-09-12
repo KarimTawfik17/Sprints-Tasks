b7347dbcf97c66c454db467aecd1573099bd2b00
Karim Tawfik <karim.tawfik17@gmail.com>
2023-09-12 11:57:06 +0300

    new contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent],
+  declarations: [AppComponent, ContactComponent, NewContactComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
